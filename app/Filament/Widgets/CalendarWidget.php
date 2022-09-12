c3e098f93ba3fb59aa6157558119c85d21baa52d
rgasch <rgasch@gmail.com>
2022-09-12 20:27:10 +0200

    Syncing config changes.

---
+++ app/Filament/Widgets/CalendarWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
+
+class CalendarWidget extends FullCalendarWidget
+{
+}
